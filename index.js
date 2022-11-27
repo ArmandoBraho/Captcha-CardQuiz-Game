4fd4456dcf412b295c5eacc437cbe4765b7a8f8e
ArmandoBraho <105724648+ArmandoBraho@users.noreply.github.com>
2022-11-27 18:58:19 +0100

    test

--- index.js
+++ index.js
@@ -10,14 +10,16 @@ const monsterAvatar = "images/orc.png"
 const monsterHealth = "10"
 const monsterDiceRoll = 4
 
-function renderCharacter(elementId, name, avatar, health, diceRoll){
-  document.getElementById(elementId).innerHTML = 
-    `<div class="character-card">
-        <h4 class="name"> ${name} </h4>
-        <img class="avatar" src="${avatar}"/>
-        <p class="health">health: <b> ${health}</b></p>
-        <div class="dice-container"><div class="dice"> ${diceRoll} </div></div>
-    </div>`;   
+function renderCharacter(elementId, name, avatar, health, diceRoll) {
+    document.getElementById(elementId).innerHTML =
+        `<div class="character-card">
+            <h4 class="name"> ${name} </h4>
+            <img class="avatar" src="${avatar}" />
+            <div class="health">health: <b> ${health} </b></div>
+            <div class="dice-container">
+                <div class="dice"> ${diceRoll} </div>
+            </div>
+        </div>`
 }
 
 renderCharacter(heroElementId, heroName, heroAvatar, heroHealth, heroDiceRoll)
